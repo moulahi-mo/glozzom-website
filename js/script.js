aa40c94b46ce50425b34b781dfe5c851b54774f5
moulahi-mo <moulahi.mo@gmail.com>
2020-11-18 01:52:19 +0100

    adding about & services pages

---
+++ js/script.js
@@ -0,0 +1,6 @@
+$('body').scrollspy({ target: '#nav1'});
+
+// card boxes page about even colors
+
+$('.about-boxes .card:even').addClass('bg-danger');
+$('.about-boxes .card:odd').addClass('bg-dark');
